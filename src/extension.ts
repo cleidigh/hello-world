493213c4d44558edc87203c3801e4dd105f2d2c1
cleidigh <cleidigh@Gmail.com>
2017-02-21 01:57:39 -0500

    changed hello message

--- src/extension.ts
+++ src/extension.ts
@@ -13,12 +13,12 @@ export function activate(context: vscode.ExtensionContext) {
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
+    // The commandId parameter must match the command field in package.json                 
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showInformationMessage('Hello World Christopher!');
     });
 
     context.subscriptions.push(disposable);
